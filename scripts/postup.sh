fb6049f40a97e876ecdbf36cf4e55e5278a34fc9
Pavel Vorobev <pavelvorobev.work@gmail.com>
2024-08-22 08:53:11 +0300

    Sync

--- scripts/postup.sh
+++ scripts/postup.sh
@@ -1,7 +1,4 @@
 SERVERPORT=5995
-ip tuntap add dev tun0 mod tun
-ip addr add 10.0.0.1/24 dev tun0
-ip link set tun0 up
 ip link set dev tun0 mtu 1500
 ifc=`route -n | awk '{if($1=="0.0.0.0" && $8 != "tun0"){print $8; exit;}}'`
 INTERFACE="$ifc"
