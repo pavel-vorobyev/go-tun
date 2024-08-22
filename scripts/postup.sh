3a8224318ba4035a0d7a48cccc809dbba69fad38
Pavel Vorobev <pavelvorobev.work@gmail.com>
2024-08-22 08:54:58 +0300

    Sync

--- scripts/postup.sh
+++ scripts/postup.sh
@@ -1,5 +1,4 @@
 SERVERPORT=5995
-ip link set dev tun0 mtu 1500
 ifc=`route -n | awk '{if($1=="0.0.0.0" && $8 != "tun0"){print $8; exit;}}'`
 INTERFACE="$ifc"
 ip=`ip addr show dev "$ifc" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }'`
