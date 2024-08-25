cb3baafdfdbb60f59c63c92ef347d09c6b1e17bb
Pavel Vorobev <pavelvorobev.work@gmail.com>
2024-08-25 19:59:06 +0300

    Sync

--- server/server.go
+++ server/server.go
@@ -72,7 +72,7 @@ func CreateServer(options *Options) (*Server, error) {
 func (s *Server) Start() {
 	s.listenConn()
 	s.listenTun()
-	s.callCallbacks()
+	//s.callCallbacks()
 }
 
 func (s *Server) listenConn() {
