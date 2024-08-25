f680cc887168a7066c9e57ef8212ab73591f2634
Pavel Vorobev <pavelvorobev.work@gmail.com>
2024-08-25 09:09:18 +0300

    Sync

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"go-tun/server"
-	"go-tun/server/packet"
 	"go-tun/util"
 	"log"
 )
 
 func main() {
 	options := server.CreateOptions()
-	options.AddRxCallback(&packet.TrafficCallback{})
-	options.AddTxCallback(&packet.TrafficCallback{})
+	//options.AddRxCallback(&packet.TrafficCallback{})
+	//ptions.AddTxCallback(&packet.TrafficCallback{})
 
 	s, err := server.CreateServer(options)
 	if err != nil {
