c1d85d120674eaa0357967ede55a59338685a22c
Pavel Vorobev <pavelvorobev.work@gmail.com>
2024-08-25 09:38:22 +0300

    Sync

--- server/server.go
+++ server/server.go
@@ -123,22 +123,24 @@ func (s *Server) listenTun() {
 func (s *Server) callCallbacks() {
 	if len(s.rxCallbacks) != 0 {
 		go func() {
-			if call := s.rxCallbackCallQueue.Pop(); call != nil {
-				log.Println(call)
-				for _, callback := range s.rxCallbacks {
-					callback.Call(call)
-				}
-			}
+			call := s.rxCallbackCallQueue.Pop()
+			log.Println(call)
+			//if call := s.rxCallbackCallQueue.Pop(); call != nil {
+			//	log.Println(call)
+			//	for _, callback := range s.rxCallbacks {
+			//		callback.Call(call)
+			//	}
+			//}
 		}()
 	}
 	if len(s.txCallbacks) != 0 {
 		go func() {
-			if call := s.txCallbackCallQueue.Pop(); call != nil {
-				log.Println(call)
-				for _, callback := range s.txCallbacks {
-					callback.Call(call)
-				}
-			}
+			//if call := s.txCallbackCallQueue.Pop(); call != nil {
+			//	log.Println(call)
+			//	for _, callback := range s.txCallbacks {
+			//		callback.Call(call)
+			//	}
+			//}
 		}()
 	}
 }
